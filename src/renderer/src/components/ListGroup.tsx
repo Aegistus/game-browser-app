ea2db50248efa3148336301140cfec3d4462ecdf
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2026-09-16 16:09:13 -0400

    Add Basic ListGroup Component

---
+++ src/renderer/src/components/ListGroup.tsx
@@ -0,0 +1,15 @@
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+
+function ListGroup()
+{
+    return(
+        <List>
+            <ListItem><h1>Item 1</h1></ListItem>
+            <ListItem><h1>Item 2</h1></ListItem>
+            <ListItem><h1>Item 3</h1></ListItem>
+            <ListItem><h1>Item 4</h1></ListItem>
+        </List>
+    ); 
+}
+export default ListGroup;
