3e2a9e99050d307e62a9df5f8256592c7295b3bc
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2026-09-17 14:27:43 -0400

    Add Props to List Group

--- src/renderer/src/components/ListGroup.tsx
+++ src/renderer/src/components/ListGroup.tsx
@@ -3,29 +3,31 @@ import ListItem from '@mui/material/ListItem'
 import { useState } from 'react';
 import '../assets/main.css'
 
-function ListGroup()
+interface Props
+{
+    items: string[];
+    heading: string;
+    onSelectItem: (item: string) => void;
+}
+
+function ListGroup({items, heading, onSelectItem}: Props)
 {
     const [selectedIndex, setSelectedIndex] = useState(0);
-    let items =
-    [
-        "New York",
-        "San Francisco",
-        "Tokyo",
-        "London",
-        "Paris"
-    ];
-
     if (items.length == 0)
     {
         return <p>No item found</p>;
     }
     return(
         <List className="List">
+            <h1>{heading}</h1>
             { items.map((item, index) => (
                 <ListItem 
                     className={selectedIndex === index ? 'ListItemActive' : 'ListItem'} 
                     key={item}
-                    onClick={() => {setSelectedIndex(index);}}
+                    onClick={() => {
+                        setSelectedIndex(index);
+                        onSelectItem(item)
+                    }}
                 >
                     <h1>{item}</h1>
                 </ListItem>) )}
